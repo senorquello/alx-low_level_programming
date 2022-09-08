b2958fccdb6d99c50889debe1dc150acc9c20457
senorquello <iamelunico01@gmail.com>
2022-09-08 13:36:46 -0700

    Advanced Task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-#include<unistd.h>
 /**
  * main - Prints standard error
  * Return: 1 if success
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59, 1, stderr");
 	return (1);
 }
