711d04f8ab94e20f56a10350ee2f018041ab2022
senorquello <iamelunico01@gmail.com>
2022-09-08 13:14:46 -0700

    Advanced Tas 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Prints standard error
+ * Return: 1 if success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korper, 2015-10-19\n", 58);
+	return (1);
+}
