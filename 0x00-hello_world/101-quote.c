c3f29edaca4f2c75e32df6b3064c8e6803de8144
senorquello <iamelunico01@gmail.com>
2022-09-08 13:29:37 -0700

    Advanced Task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korper, 2015-10-19\n", 58);
+	fwrite("and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59,1,stderr);
 	return (1);
 }
