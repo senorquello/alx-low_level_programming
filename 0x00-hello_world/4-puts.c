25fbcd66a9d1a4e0a505899e2c3a000c99c928f3
senorquello <iamelunico01@gmail.com>
2022-09-08 10:02:25 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry point
  * char Mystr - variable tostore string literals
- * puts - function to print 
+ * puts - function to print
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr);
 	return (0);
 }
