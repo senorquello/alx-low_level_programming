1992b0f3775bba9eb679e56e30da0766b3cdaefc
senorquello <iamelunico01@gmail.com>
2022-09-08 07:18:58 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
-int main(void)
+int main(void) /*main declaration*/
 {
 	/*String initialisation*/
 	char Mystr[] = "Programming is like building a multilingual puzzle";
+	
 	puts(Mystr); /*Writing the string to stdout*/
 	return 0;
 }
