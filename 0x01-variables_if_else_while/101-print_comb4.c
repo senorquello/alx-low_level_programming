33b68e2d52eedd5b56cf041f4ace50441b478a8a
senorquello <iamelunico01@gmail.com>
2022-09-09 07:37:48 -0700

    Advanced Task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if (a == '7' && p == '8')
+					if (a == '7' && b == '8')
 						continue;
 					putchar(',');
 					putchar(' ');
