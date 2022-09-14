a4ef1e1e4fe40fea198b7db73da263258612743f
senorquello <iamelunico01@gmail.com>
2022-09-13 21:49:26 -0700

    Tasks

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,9 +5,9 @@
  * @a: An integer a
  * @b: an integer b
  * Description: Function that adds two integers and returns the result
- * Return: Result to add a and b 
+ * Return: Result to add a and b
  */
 int add(int a, int b)
 {
 	return (a + b);
-}
+i}
