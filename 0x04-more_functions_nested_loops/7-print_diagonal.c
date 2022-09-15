255c693facdcd99799e47f7e6c697f72acbf2a08
senorquello <iamelunico01@gmail.com>
2022-09-15 16:53:15 -0700

    More functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,7 @@ void print_diagonal(int n)
 		for (draw = 1; draw <= n; draw++)
 		{
 				for (space = 1; space < draw; space++)
-					_putchar('');
+					_putchar(' ');
 				_putchar('\\');
 				_putchar('\n');
 		}
