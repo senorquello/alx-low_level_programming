47f08f3955abc62e17f876eacd7ce804e51d0860
senorquello <iamelunico01@gmail.com>
2022-09-15 18:38:38 -0700

    More functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,11 +4,14 @@
  * print_numbers - a function to print 0 to 9
  * Return: Always 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int i = '0';
-
-	for (; i <= '9'; i++)
-		_putchar(i);
+	char i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
 	_putchar('\n');
 }
