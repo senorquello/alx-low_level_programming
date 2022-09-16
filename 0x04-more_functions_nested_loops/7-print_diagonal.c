f39d796cd985c3a6120b6901b41cfac01539792d
senorquello <iamelunico01@gmail.com>
2022-09-15 17:17:21 -0700

    More functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,21 +4,22 @@
  * print_diagonal - print character '\' n times
  * @n: number of characters to draw
  */
+
 void print_diagonal(int n)
 {
-	int draw;
-	int space;
+	int a;
+	int b;
 
 	if (n > 0)
 	{
-		for (draw = 1; draw <= n; draw++)
+		for (a = 1; a <= n; a++)
 		{
-				for (space = 1; space < draw; space++)
-					_putchar(' ');
-				_putchar('\\');
-				_putchar('\n');
+			for (b = 1; b < a; b++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-		else
-			_putchar('\n');
+	else
+		_putchar('\n');
 }
