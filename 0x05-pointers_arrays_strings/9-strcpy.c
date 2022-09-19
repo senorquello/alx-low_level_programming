72a826df2169a208bab387acbb133c101af6c69d
senorquello <iamelunico01@gmail.com>
2022-09-19 11:34:35 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
  * @dest: A pointer to destination of string
  * @src: A pointer to source string to copy from
  *
- * Return : pointer to dest
+ * Return : aux pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
