893cdcd6e8353fcb01c1f15170131d1bd3b15422
senorquello <iamelunico01@gmail.com>
2022-09-22 13:47:28 -0700

    Task 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,10 +18,13 @@ char *_strncpy(char *dest, char *src, int n)
 		src++;
 	}
 	srclen++;
+
 	if (n > srclen)
 		n = srclen;
 	src = start;
+
 	for (; i < n; i++)
 		*dest++ = *src++;
+
 	return (temp);
 }
